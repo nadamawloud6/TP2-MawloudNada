b94be816fc3853ca8f47788d2ae561cf7a77125b
nadamawloud6 <nadamawloud6@gmail.com>
2025-11-25 12:00:41 +0100

    exp

---
+++ app/src/test/java/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun subtract_isCorrect() {
+        assertEquals(5, 8 - 3)
+    }
+}
